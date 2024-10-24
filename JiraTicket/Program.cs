55585bff7c5487a23a04271f8a30a61bfca4df54
Selva.Boopathi <elanche97@hotmail.com>
2024-10-24 17:36:01 +0530

    Updated current code

---
+++ JiraTicket/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiraTicket
+{
+    internal static class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string ProjectName = "";
+            var jsonstru = $@"{{
+    ""fields"": {{
+        ""project"": {{
+            ""key"": ""{ProjectName}""
+        }},
+        ""summary"": ""Test API"",
+        ""issuetype"": {{
+            ""name"": ""Task""  
+        }},
+        ""description"": {{
+            ""content"": [
+                {{
+                    ""content"": [
+                        {{
+                            ""text"": ""This is an detailed description for the jira ticket creation process from API 3."",
+                            ""type"": ""text""
+                        }}
+                    ],
+                    ""type"": ""paragraph""
+                }}
+            ],
+            ""type"": ""doc"",
+            ""version"": 1
+        }},
+        ""priority"": {{
+            ""name"": ""High""
+        }},
+        ""labels"": [
+            ""APITicket""
+        ]
+    }}
+}}";
+
+            var username = "xxxxxx";
+            var password = "xxxxxx";
+
+            var httpClient = new HttpClient();
+
+            var request = new HttpRequestMessage(HttpMethod.Post, "https://{xxxxxx}.atlassian.net/rest/api/3/issue")
+            {
+                Content = new StringContent(jsonstru, Encoding.UTF8, "application/json")
+            };
+
+            var base64Credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(username + ":" + password));
+            request.Headers.Authorization = new AuthenticationHeaderValue("Basic", base64Credentials);
+
+            HttpResponseMessage response = await httpClient.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+
+                Console.WriteLine(responseContent);
+            }
+        }
+    }
+}
---
+++ JiraTicket/TicketModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace JiraTicket
+{
+    public class Document
+    {
+        public List<ContentItem> Content { get; set; }
+    }
+
+    public class ContentItem
+    {
+        public string Type { get; set; }
+        public List<Content> Content { get; set; } // Allows for both text and table rows
+    }
+
+    public class Content
+    {
+        public string Type { get; set; }
+        public string Text { get; set; }
+        public List<Mark> Marks { get; set; }
+    }
+
+    public class Mark
+    {
+        public string Type { get; set; }
+    }
+
+    public class TableAttrs
+    {
+        public bool IsNumberColumnEnabled { get; set; }
+        public string Layout { get; set; }
+        public string LocalId { get; set; }
+    }
+}
